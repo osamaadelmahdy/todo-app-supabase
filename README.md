d5605a76296b3bab5189d5b7d60e71da252c9305
osama adel mahdy <osamaadelmahdy@gmail.com>
2024-10-19 00:57:02 +0300

    init app

---
+++ README.md
@@ -0,0 +1,3 @@
+# React + TypeScript + Supabase + react-query
+
+This is a todo app with login and register feat 
---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex mt-10 justify-center container mx-auto px-4 ">
+        <Home />
+      </div>
+    </>
+  );
+}
+
+export default App;
